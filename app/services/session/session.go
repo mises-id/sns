c52681dbb36e87ca7b895366eaa64a16a58ee933
xinshuaifeng <xinshuaifeng@big.one>
2021-08-09 20:32:07 +0800

    mises user debug

--- app/services/session/session.go
+++ app/services/session/session.go
@@ -22,9 +22,6 @@ func init() {
 }
 
 func SignIn(ctx context.Context, auth string) (string, error) {
-	if env.Envs.Debug {
-		return auth, nil
-	}
 	misesid, err := misesClient.Auth(auth)
 	if err != nil {
 		logrus.Errorf("mises verify error: %v", err)
